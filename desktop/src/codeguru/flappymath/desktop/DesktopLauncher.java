6f1880f5193b172a50cdf134f80bd87d03a9a1b4
Code-Apprentice <codeguru42@gmail.com>
2019-10-26 10:09:41 -0600

    Set window size

--- desktop/src/codeguru/flappymath/desktop/DesktopLauncher.java
+++ desktop/src/codeguru/flappymath/desktop/DesktopLauncher.java
@@ -7,6 +7,9 @@ import codeguru.flappymath.FlappyMathGame;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Flappy Math";
+		config.height = 203;
+		config.width = 135;
 		new LwjglApplication(new FlappyMathGame(), config);
 	}
 }
