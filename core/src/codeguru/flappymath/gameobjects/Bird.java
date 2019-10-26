46f882353d067619f8e00f23d92abf76107e59d4
Code-Apprentice <codeguru42@gmail.com>
2019-10-26 11:07:57 -0600

    Add Bird

---
+++ core/src/codeguru/flappymath/gameobjects/Bird.java
@@ -0,0 +1,55 @@
+package codeguru.flappymath.gameobjects;
+
+import com.badlogic.gdx.math.Vector2;
+
+public class Bird {
+    private Vector2 position;
+    private Vector2 velocity;
+    private Vector2 acceleration;
+
+    private float rotation;
+    private int width;
+    private int height;
+
+    public Bird(float x, float y, int width, int height) {
+        this.position = new Vector2(x, y);
+        this.width = width;
+        this.height = height;
+        this.velocity = new Vector2(0, 0);
+        this.acceleration = new Vector2(0, 460);
+    }
+
+    public void update(float delta) {
+        velocity.add(acceleration.cpy().scl(delta));
+
+        if (velocity.y > 200) {
+            velocity.y = 200;
+        }
+
+        position.add(velocity.cpy().scl(delta));
+    }
+
+    public void onClick() {
+        velocity.y = -140;
+    }
+
+    public float getX() {
+        return position.x;
+    }
+
+    public float getY() {
+        return position.y;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getHeight() {
+        return height;
+    }
+
+    public float getRotation() {
+        return rotation;
+    }
+}
