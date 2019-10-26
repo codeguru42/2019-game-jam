f37dabf379bcd6aab5bba608cefa7db07a92c8f8
Code-Apprentice <codeguru42@gmail.com>
2019-10-26 10:12:24 -0600

    Assets

--- core/src/codeguru/flappymath/FlappyMathGame.java
+++ core/src/codeguru/flappymath/FlappyMathGame.java
@@ -13,7 +13,7 @@ public class FlappyMathGame extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		img = new Texture("bird.png");
 	}
 
 	@Override
