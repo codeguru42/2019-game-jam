b1b85018502956a047509be76ec10ff6c782e807
Code-Apprentice <codeguru42@gmail.com>
2019-10-26 10:38:46 -0600

    Render a rectangle

--- core/src/codeguru/flappymath/FlappyMathRenderer.java
+++ core/src/codeguru/flappymath/FlappyMathRenderer.java
@@ -1,13 +1,30 @@
 package codeguru.flappymath;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
+import com.badlogic.gdx.math.Rectangle;
+
 public class FlappyMathRenderer {
     private final FlappyMathWorld world;
+    private OrthographicCamera camera = new OrthographicCamera();
+    private ShapeRenderer renderer = new ShapeRenderer();
 
     public FlappyMathRenderer(FlappyMathWorld world) {
         this.world = world;
+        this.camera.setToOrtho(true, 135, 203);
+        this.renderer.setProjectionMatrix(camera.combined);
     }
 
     public void render() {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+        renderer.begin(ShapeRenderer.ShapeType.Filled);
+        renderer.setColor(87 / 255.0f, 109 / 255.0f, 120 / 255.0f, 1);
+        Rectangle rect = world.getRect();
+        renderer.rect(rect.x, rect.y, rect.width, rect.height);
+        renderer.end();
     }
 }
--- core/src/codeguru/flappymath/FlappyMathWorld.java
+++ core/src/codeguru/flappymath/FlappyMathWorld.java
@@ -1,7 +1,18 @@
 package codeguru.flappymath;
 
-public class FlappyMathWorld {
-    public void update(float delta) {
+import com.badlogic.gdx.math.Rectangle;
 
+public class FlappyMathWorld {
+    private Rectangle rect = new Rectangle(0, 0, 17, 12);
+
+    public void update(float delta) {
+        rect.x++;
+        if (rect.x > 137) {
+            rect.x = 0;
+        }
+    }
+
+    public Rectangle getRect() {
+        return rect;
     }
 }
