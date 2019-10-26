a901add56595dce05b6dfeeb6e3a504679d92a55
Code-Apprentice <codeguru42@gmail.com>
2019-10-26 10:10:13 -0600

    Scale a little larger

--- desktop/src/codeguru/flappymath/desktop/DesktopLauncher.java
+++ desktop/src/codeguru/flappymath/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Flappy Math";
-		config.height = 203;
-		config.width = 135;
+		config.height = 203*3;
+		config.width = 135*3;
 		new LwjglApplication(new FlappyMathGame(), config);
 	}
 }
